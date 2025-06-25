f3f2405c187a007bcccb4638a724766651013d8e
Takahiro Nagasawa <takahiro.nagasawa@proton.me>
2025-06-25 11:19:31 +0200

    Fix linter issues
    Signed-off-by: Takahiro Nagasawa <takahiro.nagasawa@proton.me>

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from "tailwindcss-animate";
 import type { Config } from "tailwindcss";
 
 const config = {
@@ -87,7 +88,7 @@ const config = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
 
 export default config;
